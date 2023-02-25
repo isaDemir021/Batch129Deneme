e0364ab792a906544b9b4e0f40ee8176541b0dc2
Isa DEMIR <isademir4521@gmail.com>
2023-02-25 17:28:16 +0100

    V3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,6 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         // GitHub uzerinden guncellemeyi intellij eye cekme ...
+        System.out.println("Yeni bir seyler ekleyelim  ..");
   
     }
 }
