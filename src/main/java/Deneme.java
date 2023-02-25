36453974bdf1fba137eed0d9f8e2a879bf204872
isaDemir021 <121893090+isaDemir021@users.noreply.github.com>
2023-02-25 17:40:30 +0100

    Update Deneme.java

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -4,6 +4,7 @@ public class Deneme {
         // GitHub uzerinden guncellemeyi intellij eye cekme ...
         System.out.println("Yeni bir seyler ekleyelim  ..");
     //Merhaba yeni bir dunya kurmak gerek ..
+        // GitHup 
 
         // Girtttt
     }
