9f17c8ed11f181f3b029879cc54373dd21ad3d77
Isa DEMIR <isademir4521@gmail.com>
2023-02-25 17:31:48 +0100

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -4,5 +4,7 @@ public class Deneme {
         // GitHub uzerinden guncellemeyi intellij eye cekme ...
         System.out.println("Yeni bir seyler ekleyelim  ..");
     //Merhaba yeni bir dunya kurmak gerek ..
+
+        // Girtttt
     }
 }
